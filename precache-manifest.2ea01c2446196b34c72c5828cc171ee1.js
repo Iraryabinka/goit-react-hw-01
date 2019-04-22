b544bcf71b620b3afd37b5eea1bc3c2f07f874b1
Irina Chervyakova <irinachervaykova@gmail.com>
2019-04-22 15:21:36 +0300

    Updates

---
+++ precache-manifest.2ea01c2446196b34c72c5828cc171ee1.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "4700f87e9ea10d4e4006",
+    "url": "/goit-react-hw-01/static/js/main.12ac6f8d.chunk.js"
+  },
+  {
+    "revision": "f51632aec18d5636ad74",
+    "url": "/goit-react-hw-01/static/js/runtime~main.bdf556df.js"
+  },
+  {
+    "revision": "3f6bf91415e9a728f374d3fd4217b03e",
+    "url": "/goit-react-hw-01/index.html"
+  }
+];
