53a31f03f20eb728844752462f7c98d5872c45ff
Irina Chervyakova <irinachervaykova@gmail.com>
2019-04-24 17:59:11 +0300

    new commit

---
+++ my-app/src/components/Profile.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import user from '../products.json';
+
+const Profile = ({ name, tag, location, avatar, stats }) => (
+  <div>
+    <div>
+      <img src={user.avatar} alt="user avatar" height={200} />
+      <p>{user.name}</p>
+      <p>{user.tag}</p>
+      <p>{user.location}</p>
+    </div>
+
+    <ul>
+      <li>
+        <span>Followers </span>
+        <span>{user.stats.followers}</span>
+      </li>
+      <li>
+        <span>Views </span>
+        <span>{user.stats.views}</span>
+      </li>
+      <li>
+        <span>Likes </span>
+        <span>{user.stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+Profile.defaultProps = {
+  alt: 'product image',
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.number.isRequired,
+};
+
+export default Profile;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,1 +1,6 @@
-//import React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import user from './products.json';
+import Profile from './components/Profile';
+
+ReactDOM.render(<Profile user={user} />, document.getElementById('root'));
